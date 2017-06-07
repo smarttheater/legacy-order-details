19b7ddf251d3050d57bc6823890943772bef9636
Kusunose <kusunose@motionpicture.jp>
2017-06-07 10:21:52 +0900

    TTS-29 キャンセルテスト用ポップアップ追加

--- public/js/inquiry/result.js
+++ public/js/inquiry/result.js
@@ -1,10 +1,43 @@
 $(function () {
+    // ＱＲコード表示
     $('.qrcode').each(function(index) {
         $(this).html("");
         $(this).qrcode({
-            width:100,
-            height:100,    
-            text:this.id
+            width: 100,
+            height: 100,    
+            text: this.id
         });
     });
+
+    // キャンセル
+    $(document).on('click', '.btn-cancel', function (event) {
+        event.preventDefault();
+        cancel();
+    });
 });
+/**
+ * キャンセル
+ * @function cancel
+ * @returns {void}
+ */
+function cancel() {
+    // var theater = $('select[name=theater]').val();
+    // var day = $('select[name=day]').val();
+    // if (!theater || !day) {
+    //     alert('劇場、上映日を選択してください');
+    //     return;
+    // }
+    // var modal = $('#newModal');
+    // modal.find('.film-name').text('未選択');
+    // modal.find('.film-name').attr('data-film-id', '');
+    // modal.find('input[name=film]').val('');
+    // modal.find('select[name=openTimeHour]').val('00');
+    // modal.find('select[name=openTimeMinutes]').val('00');
+    // modal.find('select[name=startTimeHour]').val('00');
+    // modal.find('select[name=startTimeMinutes]').val('00');
+    // modal.find('select[name=endTimeHour]').val('00');
+    // modal.find('select[name=endTimeMinutes]').val('00');
+    // modal.find('select[name=screen]').val('');
+    // modal.find('select[name=ticketTypeGroup]').val('');
+    $('#cancelModal').modal();
+}
