eee62ffa7fe0c655b7823eb5217c37c3ac41f3c6
Kusunose <kusunose@motionpicture.jp>
2017-07-03 09:45:25 +0900

    TTTS-37 予約通過確認api動作確認版(confing使用)

--- app/controllers/checkIn.js
+++ app/controllers/checkIn.js
@@ -18,10 +18,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ttts_domain_1 = require("@motionpicture/ttts-domain");
 const ttts_domain_2 = require("@motionpicture/ttts-domain");
 const ttts_domain_3 = require("@motionpicture/ttts-domain");
+const conf = require("config");
 const moment = require("moment");
 const _ = require("underscore");
-const checkpointNames = { 'daiten-auth': '大展望台', 'topdeck-auth': 'TOP DESK' };
-const ticketsExtra = ['000098', '000099'];
+//const checkpointNames = {'daiten-auth': '大展望台', 'topdeck-auth': 'TOP DESK'};
+//const ticketsExtra: string[]  = ['000098', '000099'];
+const checkpointNames = conf.get('checkpointNames');
+const ticketsExtra = conf.get('ticketsExtra');
 /**
  * 入場画面のパフォーマンス検索
  * @memberof checkIn
@@ -159,16 +162,18 @@ exports.getReservations = getReservations;
 function getPassList(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            if (!req) {
-                throw new Error('UnexpectedError');
-            }
+            //req.query('day');
             // 対象日時セット(引数化の可能性あり)
             //@@@@@@@@@@@
+            //const selectType: string = 'day';
             const selectType = 'time';
             // 現在時刻取得
             // データがないのでテストで9時
             //const now = moment();
-            const now = moment('20170619 0900', 'YYYYMMDD HHmm');
+            let now = moment('20170619 0900', 'YYYYMMDD HHmm');
+            if (req.query.day !== undefined) {
+                now = moment(req.query.day, 'YYYYMMDD');
+            }
             //---
             // 取得対象の日付と開始時刻FromToを取得
             const timeInfo = yield getStartTime(selectType, now);
@@ -192,9 +197,18 @@ function getPassList(req, res) {
             //   performance_id1:{ 'daiten-auth' : [res1.checkins[0],res2.checkins[0]],
             //                     'topdeck-auth': [res1.checkins[1]] },
             // }
-            const dataCheckins = groupingCheckinsByPerformanceAndWhere(dataByPerformance);
+            //   performance_id1:{ 'daiten-auth' : {
+            //                        checkins: [res1.checkins[0],res2.checkins[0]],
+            //                        arrived: { '00099': 1, '00098': 2, },
+            //                     'topdeck-auth': {
+            //                        checkins: [res1.checkins[1],
+            //                        arrived: { '00099': 0, '00098': 1, },
+            const dataCheckins = groupingCheckinsByWhere(dataByPerformance);
             // レスポンス編集
-            const data = { schedules: [] };
+            const data = {
+                checkpoints: checkpointNames,
+                schedules: []
+            };
             Object.keys(dataByPerformance).forEach((performanceId) => {
                 // パフォーマンス情報セット
                 const performance = dataByPerformance[performanceId].performance;
@@ -209,7 +223,7 @@ function getPassList(req, res) {
                     concernedReservedArray: [],
                     checkpointArray: []
                 };
-                // 特殊チケット情報セット
+                // 特殊チケット予約情報セット
                 const concernedReservedArray = [];
                 const reservedExtra = dataByPerformance[performanceId].reservedExtra;
                 Object.keys(reservedExtra).forEach((id) => {
@@ -220,25 +234,24 @@ function getPassList(req, res) {
                     });
                 });
                 schedule.concernedReservedArray = concernedReservedArray;
-                // パフォーマンス+通過地点単位にチェックイン情報をグルーピング
-                // dataCheckins = {
-                //   performance_id1:{ 'daiten-auth' : [res1.checkins[0],res2.checkins[0]],
-                //                     'topdeck-auth': [res1.checkins[1]] },
-                // }
                 // チェックイン情報セット
                 const checkins = dataCheckins[performanceId];
                 if (Object.keys(checkins).length > 0) {
-                    const checkpoint = {};
-                    // {'daiten-auth' : [res1.checkins[0],res2.checkins[0]],･･･}
+                    //const checkpoint: any = {concernedUnarrivedArray: []};
+                    //   performance_id1:{ 'daiten-auth' : {
+                    //                        checkins: [res1.checkins[0],res2.checkins[0]],
+                    //                        arrived: { '00099': 1, '00098': 2, },
                     Object.keys(checkins).forEach((where) => {
-                        checkpoint.id = where;
-                        checkpoint.name = (checkpointNames.hasOwnProperty(where)) ? checkpointNames[where] : where;
-                        checkpoint.unarrivedNum = reservedNum - checkins[where].length;
+                        const checkpoint = {
+                            id: where,
+                            name: (checkpointNames.hasOwnProperty(where)) ? checkpointNames[where] : where,
+                            unarrivedNum: reservedNum - checkins[where].checkins.length
+                        };
                         // チェックポイントを通過した予約情報の中で特殊チケットの内訳セット
                         const concernedUnarrivedArray = getConcernedUnarrivedArray(concernedReservedArray, checkins[where]);
                         checkpoint.concernedUnarrivedArray = concernedUnarrivedArray;
+                        schedule.checkpointArray.push(checkpoint);
                     });
-                    schedule.checkpointArray.push(checkpoint);
                 }
                 data.schedules.push(schedule);
             });
@@ -377,7 +390,7 @@ function groupingReservationsByPerformance(dicPerformances, reservations) {
                     };
                 }
                 else {
-                    reservedExtra.reservedNum += 1;
+                    reservedExtra[reservation.ticket_type].reservedNum += 1;
                 }
             }
             else {
@@ -398,29 +411,47 @@ function getTicketTypes(group) {
     return __awaiter(this, void 0, void 0, function* () {
         // 券種取得(ticket_typesをjoinして名称etcも取得)
         const ticketTypeGroup = yield ttts_domain_1.Models.TicketTypeGroup.findOne({ _id: group }).populate('ticket_types').exec();
-        return (ticketTypeGroup) ? ticketTypeGroup.get('ticket_types') : null;
+        return (ticketTypeGroup !== null) ? ticketTypeGroup.get('ticket_types') : null;
     });
 }
 /**
  * 予約通過確認・パフォーマンス+通過地点単位にチェックイン情報をグルーピング
  * @memberof checkIn
- * @function groupingCheckinsByPerformanceAndWhere
+ * @function groupingCheckinsByWhere
  * @param {any} dataByPerformance
  * @returns {Promise<any>}
  */
-function groupingCheckinsByPerformanceAndWhere(dataByPerformance) {
+function groupingCheckinsByWhere(dataByPerformance) {
+    // dataCheckins = {
+    //   performance_id1:{ 'daiten-auth' : [res1.checkins[0],res2.checkins[0]],
+    //                     'topdeck-auth': [res1.checkins[1]] },
+    // }
+    //   performance_id1:{ 'daiten-auth' : {
+    //                        checkins: [res1.checkins[0],res2.checkins[0]],
+    //                        arrived: { '00099': 1, '00098': 2, },
+    //                     'topdeck-auth': {
+    //                        checkins: [res1.checkins[1],
+    //                        arrived: { '00099': 0, '00098': 1, },
     const dataCheckins = {};
     Object.keys(dataByPerformance).forEach((performanceId) => {
         const dataCheckin = {};
         dataByPerformance[performanceId].reservations.forEach((reservation) => {
             reservation.checkins.forEach((checkin) => {
                 if (!dataCheckin.hasOwnProperty(checkin.where)) {
-                    dataCheckin[checkin.where] = [];
+                    //dataCheckin[checkin.where] = [];
+                    dataCheckin[checkin.where] = { checkins: [], arrived: {} };
                 }
-                checkin.id = reservation._id;
-                checkin.ticket_type = reservation.ticket_type;
-                checkin.ticket_type_name = reservation.ticket_type_name;
-                dataCheckin[checkin.where].push(checkin);
+                if (!dataCheckin[checkin.where].arrived.hasOwnProperty(reservation.ticket_type)) {
+                    dataCheckin[checkin.where].arrived[reservation.ticket_type] = 1;
+                }
+                else {
+                    dataCheckin[checkin.where].arrived[reservation.ticket_type] += 1;
+                }
+                checkin.id = reservation._id; // 予約id
+                checkin.ticket_type = reservation.ticket_type; // ticket_type(00099)
+                checkin.ticket_type_name = reservation.ticket_type_name; // ticket_type(車椅子)
+                //dataCheckin[checkin.where].push(checkin);
+                dataCheckin[checkin.where].checkins.push(checkin);
             });
         });
         dataCheckins[performanceId] = dataCheckin;
@@ -444,7 +475,19 @@ function getStatusCount(reservations, status) {
     });
     return statusNum;
 }
-function getConcernedUnarrivedArray(concernedReservedArray, checkins) {
+/**
+ * 予約通過確認・特殊チケットごとの未入場者数取得
+ * @memberof checkIn
+ * @function getConcernedUnarrivedArray
+ * @param {any[]} concernedReservedArray
+ * @param {any[]} checkins
+ * @returns {any[]}
+ */
+function getConcernedUnarrivedArray(concernedReservedArray, checkin) {
+    //   checkin('daiten-auth') = {
+    //        checkins: [res1.checkins[0],res2.checkins[0]],
+    //        arrived: { '00099': 1, '00098': 2, }
+    //  }
     const concernedUnarrivedArray = [];
     // 特殊チケットごとの来場予定者をセット
     concernedReservedArray.forEach((reserve) => {
@@ -454,9 +497,10 @@ function getConcernedUnarrivedArray(concernedReservedArray, checkins) {
             unarrivedNum: reserve.reservedNum //初期値は来場予定者数
         });
     });
-    // 未入場者=来場予定者数-チェックポイント通過者数
+    // 未入場者数 = 来場予定者数 - チェックポイント通過者数
     concernedUnarrivedArray.forEach((unarrive) => {
-        unarrive.unarrivedNum = unarrive.unarrivedNum - checkins.length;
+        const arrivedNum = checkin.arrived.hasOwnProperty(unarrive.id) ? checkin.arrived[unarrive.id] : 0;
+        unarrive.unarrivedNum = unarrive.unarrivedNum - arrivedNum;
     });
     return concernedUnarrivedArray;
 }
--- app/controllers/checkIn.ts
+++ app/controllers/checkIn.ts
@@ -8,13 +8,16 @@
 import { Models } from '@motionpicture/ttts-domain';
 import { ReservationUtil } from '@motionpicture/ttts-domain';
 import { FilmUtil } from '@motionpicture/ttts-domain';
+import * as conf from 'config';
 import { NextFunction, Request, Response } from 'express';
 import * as moment from 'moment';
 import * as mongoose from 'mongoose';
 import * as _ from 'underscore';
 
-const checkpointNames = {'daiten-auth': '大展望台', 'topdeck-auth': 'TOP DESK'};
-const ticketsExtra: string[]  = ['000098', '000099'];
+//const checkpointNames = {'daiten-auth': '大展望台', 'topdeck-auth': 'TOP DESK'};
+//const ticketsExtra: string[]  = ['000098', '000099'];
+const checkpointNames = conf.get<any>('checkpointNames');
+const ticketsExtra: string[]  = conf.get<string[]>('ticketsExtra');
 
 /**
  * 入場画面のパフォーマンス検索
@@ -160,16 +163,18 @@ export async function getReservations(req: Request, res: Response): Promise<void
 // tslint:disable-next-line:max-func-body-length
 export async function getPassList(req: Request, res: Response): Promise<void> {
     try {
-        if (!req) {
-            throw new Error('UnexpectedError');
-        }
+        //req.query('day');
         // 対象日時セット(引数化の可能性あり)
         //@@@@@@@@@@@
+        //const selectType: string = 'day';
         const selectType: string = 'time';
         // 現在時刻取得
         // データがないのでテストで9時
         //const now = moment();
-        const now = moment('20170619 0900', 'YYYYMMDD HHmm');
+        let now = moment('20170619 0900', 'YYYYMMDD HHmm');
+        if (req.query.day !== undefined) {
+            now = moment(req.query.day, 'YYYYMMDD');
+        }
         //---
 
         // 取得対象の日付と開始時刻FromToを取得
@@ -198,10 +203,19 @@ export async function getPassList(req: Request, res: Response): Promise<void> {
         //   performance_id1:{ 'daiten-auth' : [res1.checkins[0],res2.checkins[0]],
         //                     'topdeck-auth': [res1.checkins[1]] },
         // }
-        const dataCheckins: any = groupingCheckinsByPerformanceAndWhere(dataByPerformance);
+        //   performance_id1:{ 'daiten-auth' : {
+        //                        checkins: [res1.checkins[0],res2.checkins[0]],
+        //                        arrived: { '00099': 1, '00098': 2, },
+        //                     'topdeck-auth': {
+        //                        checkins: [res1.checkins[1],
+        //                        arrived: { '00099': 0, '00098': 1, },
+        const dataCheckins: any = groupingCheckinsByWhere(dataByPerformance);
 
         // レスポンス編集
-        const data: any = {schedules: []};
+        const data: any = {
+            checkpoints: checkpointNames,
+            schedules: []
+        };
         Object.keys(dataByPerformance).forEach((performanceId) => {
             // パフォーマンス情報セット
             const performance: any = dataByPerformance[performanceId].performance;
@@ -216,7 +230,7 @@ export async function getPassList(req: Request, res: Response): Promise<void> {
                 concernedReservedArray: [],
                 checkpointArray: []
             };
-            // 特殊チケット情報セット
+            // 特殊チケット予約情報セット
             const concernedReservedArray: any[] = [];
             const reservedExtra: any = dataByPerformance[performanceId].reservedExtra;
             Object.keys(reservedExtra).forEach((id) => {
@@ -228,26 +242,24 @@ export async function getPassList(req: Request, res: Response): Promise<void> {
             });
             schedule.concernedReservedArray = concernedReservedArray;
 
-            // パフォーマンス+通過地点単位にチェックイン情報をグルーピング
-            // dataCheckins = {
-            //   performance_id1:{ 'daiten-auth' : [res1.checkins[0],res2.checkins[0]],
-            //                     'topdeck-auth': [res1.checkins[1]] },
-            // }
-
             // チェックイン情報セット
             const checkins = dataCheckins[performanceId];
             if (Object.keys(checkins).length > 0) {
-                const checkpoint: any = {};
-                // {'daiten-auth' : [res1.checkins[0],res2.checkins[0]],･･･}
+                //const checkpoint: any = {concernedUnarrivedArray: []};
+                //   performance_id1:{ 'daiten-auth' : {
+                //                        checkins: [res1.checkins[0],res2.checkins[0]],
+                //                        arrived: { '00099': 1, '00098': 2, },
                 Object.keys(checkins).forEach((where) => {
-                    checkpoint.id = where;
-                    checkpoint.name = (checkpointNames.hasOwnProperty(where)) ? (<any>checkpointNames)[where] : where;
-                    checkpoint.unarrivedNum = reservedNum - checkins[where].length;
+                    const checkpoint: any = {
+                        id: where,
+                        name: (checkpointNames.hasOwnProperty(where)) ? (<any>checkpointNames)[where] : where,
+                        unarrivedNum: reservedNum - checkins[where].checkins.length
+                    };
                     // チェックポイントを通過した予約情報の中で特殊チケットの内訳セット
                     const concernedUnarrivedArray: any[] = getConcernedUnarrivedArray(concernedReservedArray, checkins[where]);
                     checkpoint.concernedUnarrivedArray = concernedUnarrivedArray;
+                    schedule.checkpointArray.push(checkpoint);
                 });
-                schedule.checkpointArray.push(checkpoint);
             }
             data.schedules.push(schedule);
         });
@@ -293,7 +305,7 @@ async function getStartTime(selectType: string, now: moment.Moment ) : Promise<a
     });
     // 3パフォーマンス目の開始時刻セット
     const startTimeFrom = performances[0].start_time;
-    const startDate = moment(performances[0].day + startTimeFrom, 'YYYYMMDDHHmm');
+    const startDate = moment(<string>performances[0].day + <string>startTimeFrom, 'YYYYMMDDHHmm');
     // tslint:disable-next-line:no-magic-numbers
     const addTime = 15 * (3 - 1);
     const startTimeTo =  moment(startDate).add('minutes', addTime).format('HHmm');
@@ -385,7 +397,7 @@ async function groupingReservationsByPerformance(dicPerformances: any[], reserva
                     reservedNum: 1
                 };
             } else {
-                (<any>reservedExtra).reservedNum += 1;
+                reservedExtra[reservation.ticket_type].reservedNum += 1;
             }
         } else {
             (<any>dataByPerformance)[keyValue].reservedNormalNum += 1;
@@ -407,28 +419,45 @@ async function getTicketTypes(group: string): Promise<any> {
         { _id: group }
     ).populate('ticket_types').exec();
 
-    return (ticketTypeGroup) ? ticketTypeGroup.get('ticket_types') : null;
+    return (ticketTypeGroup !== null) ? ticketTypeGroup.get('ticket_types') : null;
 }
 /**
  * 予約通過確認・パフォーマンス+通過地点単位にチェックイン情報をグルーピング
  * @memberof checkIn
- * @function groupingCheckinsByPerformanceAndWhere
+ * @function groupingCheckinsByWhere
  * @param {any} dataByPerformance
  * @returns {Promise<any>}
  */
-function groupingCheckinsByPerformanceAndWhere(dataByPerformance: any): any {
+function groupingCheckinsByWhere(dataByPerformance: any): any {
+    // dataCheckins = {
+    //   performance_id1:{ 'daiten-auth' : [res1.checkins[0],res2.checkins[0]],
+    //                     'topdeck-auth': [res1.checkins[1]] },
+    // }
+    //   performance_id1:{ 'daiten-auth' : {
+    //                        checkins: [res1.checkins[0],res2.checkins[0]],
+    //                        arrived: { '00099': 1, '00098': 2, },
+    //                     'topdeck-auth': {
+    //                        checkins: [res1.checkins[1],
+    //                        arrived: { '00099': 0, '00098': 1, },
     const dataCheckins: any = {};
     Object.keys(dataByPerformance).forEach((performanceId) => {
         const dataCheckin: any = {};
         (<any>dataByPerformance[performanceId].reservations).forEach((reservation: any) => {
             (<any>reservation.checkins).forEach((checkin: any) => {
                 if (!dataCheckin.hasOwnProperty(checkin.where)) {
-                    dataCheckin[checkin.where] = [];
+                    //dataCheckin[checkin.where] = [];
+                    dataCheckin[checkin.where] = {checkins: [], arrived: {}};
                 }
-                checkin.id = reservation._id;
-                checkin.ticket_type = reservation.ticket_type;
-                checkin.ticket_type_name = reservation.ticket_type_name;
-                dataCheckin[checkin.where].push(checkin);
+                if (!dataCheckin[checkin.where].arrived.hasOwnProperty(reservation.ticket_type)) {
+                    dataCheckin[checkin.where].arrived[reservation.ticket_type] = 1;
+                } else {
+                    dataCheckin[checkin.where].arrived[reservation.ticket_type] += 1;
+                }
+                checkin.id = reservation._id;                   // 予約id
+                checkin.ticket_type = reservation.ticket_type;  // ticket_type(00099)
+                checkin.ticket_type_name = reservation.ticket_type_name;    // ticket_type(車椅子)
+                //dataCheckin[checkin.where].push(checkin);
+                dataCheckin[checkin.where].checkins.push(checkin);
             });
         });
         dataCheckins[performanceId] = dataCheckin;
@@ -454,8 +483,19 @@ function getStatusCount(reservations: any[], status: string): number {
 
     return statusNum;
 }
-
-function getConcernedUnarrivedArray(concernedReservedArray: any[], checkins: any[]): any {
+/**
+ * 予約通過確認・特殊チケットごとの未入場者数取得
+ * @memberof checkIn
+ * @function getConcernedUnarrivedArray
+ * @param {any[]} concernedReservedArray
+ * @param {any[]} checkins
+ * @returns {any[]}
+ */
+function getConcernedUnarrivedArray(concernedReservedArray: any[], checkin: any): any {
+    //   checkin('daiten-auth') = {
+    //        checkins: [res1.checkins[0],res2.checkins[0]],
+    //        arrived: { '00099': 1, '00098': 2, }
+    //  }
     const concernedUnarrivedArray: any[] = [];
     // 特殊チケットごとの来場予定者をセット
     concernedReservedArray.forEach((reserve: any) => {
@@ -465,9 +505,10 @@ function getConcernedUnarrivedArray(concernedReservedArray: any[], checkins: any
             unarrivedNum: reserve.reservedNum   //初期値は来場予定者数
         });
     });
-    // 未入場者=来場予定者数-チェックポイント通過者数
+    // 未入場者数 = 来場予定者数 - チェックポイント通過者数
     concernedUnarrivedArray.forEach((unarrive: any) => {
-        unarrive.unarrivedNum = unarrive.unarrivedNum - checkins.length;
+        const arrivedNum : number = checkin.arrived.hasOwnProperty(unarrive.id) ? (<any>checkin.arrived)[unarrive.id] : 0;
+        unarrive.unarrivedNum = unarrive.unarrivedNum - arrivedNum;
     });
 
     return concernedUnarrivedArray;
