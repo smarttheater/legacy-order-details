32bd1168a8fd16e20b0f6805fa689580b8f5ba53
hoshida <h.hoshida@motionpicture.jp>
2019-04-01 19:32:51 +0900

    TTTS-494 QRコードをvanillaQRでの実装

--- public/js/common.js
+++ public/js/common.js
@@ -13,54 +13,6 @@ window.ttts.setSessionStorage = function(key, value) {
     }
 };
 
-/**
- * QRコード表示
- * @function showQRCode
- * @returns {void}
- */
-function showQRCode() {
-    $('.codeimg-barcode').each(function(index, element) {
-        var target = $(element);
-        var url = target.attr('data-qrcode');
-        var code = createQRCode(url, {
-            alt: 'QRコード'
-        });
-        target.append(code);
-    });
-}
-
-/**
- * QRコード生成
- * @function createQRCode
- * @param {string} url QRコードURL
- * @param {any} options オプション
- * @param {number} options.width 幅
- * @param {number} options.height 高さ
- * @param {string} options.alt alt
- * @param {string} options.ext 形式
- * @returns {HTMLImageElement} QR画像
- */
-function createQRCode(url, options) {
-    options = options || {};
-    var width = (options.width !== undefined) ? options.width : 100;
-    var height = (options.height !== undefined) ? options.height : 100;
-    var alt = (options.alt !== undefined) ? options.alt : '';
-    var ext = (options.ext !== undefined) ? options.ext : 'png';
-    // QR
-    var qr = new VanillaQR({
-        url: url,
-        width: width,
-        height: height,
-        colorLight: '#FFF',
-        colorDark: '#000',
-        noBorder: true
-    });
-    var image = qr.toImage(ext);
-    image.width = width;
-    image.height = height;
-    image.alt = alt;
-    return image;
-}
 
 $(function() {
     var $window = $(window);
@@ -99,5 +51,4 @@ $(function() {
         }, 300);
     });
 
-    showQRCode();
 });
