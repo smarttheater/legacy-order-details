31928ae6533dd35e0427cbec65a197bc0da88ffd
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-10-24 23:34:05 +0000

    ### Changed - 印刷トークンに注文情報が存在する場合、注文承認でコードを発行するように調整

--- dst/app/routes/reservations.js
+++ dst/app/routes/reservations.js
@@ -38,23 +38,75 @@ const orderService = new cinerinoapi.service.Order({
  * チケット印刷(A4)
  * output:thermal→PCサーマル印刷 (WindowsでStarPRNTドライバを使用)
  */
-reservationsRouter.get('/print', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+reservationsRouter.get('/print', 
+// tslint:disable-next-line:max-func-body-length
+(req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         // tslint:disable-next-line:no-suspicious-comment
         // TODO トークン期限チェック
         // 他所からリンクされてくる時のためURLで言語を指定できるようにしておく (TTTS-230)
         req.session.locale = req.params.locale;
+        let orders;
+        let reservations;
         jwt.verify(req.query.token, process.env.TTTS_TOKEN_SECRET, (jwtErr, decoded) => __awaiter(void 0, void 0, void 0, function* () {
             if (jwtErr instanceof Error) {
                 next(jwtErr);
             }
             else {
+                // 指定された予約ID
                 const ids = decoded.object;
-                let reservations = yield Promise.all(ids.map((id) => __awaiter(void 0, void 0, void 0, function* () { return reservationService.findById({ id }); })));
-                reservations = reservations.filter((r) => r.reservationStatus === tttsapi.factory.chevre.reservationStatusType.ReservationConfirmed);
-                if (reservations.length === 0) {
-                    next(new Error(req.__('NotFound')));
-                    return;
+                // decoded.reservationsが存在する場合に対応する
+                if (Array.isArray(decoded.orders)) {
+                    // 注文番号と確認番号で注文照会
+                    const printingOrders = decoded.orders;
+                    orders = yield Promise.all(printingOrders.map((printingOrder) => __awaiter(void 0, void 0, void 0, function* () {
+                        const findOrderResult = yield orderService.findByConfirmationNumber({
+                            confirmationNumber: String(printingOrder.confirmationNumber),
+                            orderNumber: String(printingOrder.orderNumber)
+                        });
+                        if (Array.isArray(findOrderResult)) {
+                            return findOrderResult[0];
+                        }
+                        else {
+                            return findOrderResult;
+                        }
+                    })));
+                    // 注文承認
+                    yield Promise.all(orders.map((order) => __awaiter(void 0, void 0, void 0, function* () {
+                        yield orderService.authorize({
+                            object: {
+                                orderNumber: order.orderNumber,
+                                customer: { telephone: order.customer.telephone }
+                            },
+                            result: {
+                                expiresInSeconds: inquiry_1.CODE_EXPIRES_IN_SECONDS
+                            }
+                        });
+                    })));
+                    // 予約リストを抽出
+                    reservations = orders.reduce((a, b) => {
+                        const reservationsByOrder = b.acceptedOffers
+                            // 指定された予約IDに絞る
+                            .filter((offer) => {
+                            return ids.includes(offer.itemOffered.id);
+                        })
+                            .map((offer) => {
+                            var _a;
+                            const unitPriceSpec = offer.priceSpecification.priceComponent[0];
+                            const itemOffered = offer.itemOffered;
+                            // 注文データのticketTypeに単価仕様が存在しないので、補完する
+                            return Object.assign(Object.assign({}, itemOffered), { paymentNo: b.confirmationNumber, paymentMethod: (_a = b.paymentMethods[0]) === null || _a === void 0 ? void 0 : _a.name, reservedTicket: Object.assign(Object.assign({}, itemOffered.reservedTicket), { ticketType: Object.assign(Object.assign({}, itemOffered.reservedTicket.ticketType), { priceSpecification: unitPriceSpec }) }) });
+                        });
+                        return [...a, ...reservationsByOrder];
+                    }, []);
+                }
+                else {
+                    reservations = yield Promise.all(ids.map((id) => __awaiter(void 0, void 0, void 0, function* () { return reservationService.findById({ id }); })));
+                    reservations = reservations.filter((r) => r.reservationStatus === tttsapi.factory.chevre.reservationStatusType.ReservationConfirmed);
+                    if (reservations.length === 0) {
+                        next(new Error(req.__('NotFound')));
+                        return;
+                    }
                 }
                 renderPrintFormat(req, res)({ reservations });
             }
@@ -107,10 +159,11 @@ reservationsRouter.get('/printByOrderNumber', (req, res, next) => __awaiter(void
             }
         });
         reservations = order.acceptedOffers.map((offer) => {
+            var _a;
             const unitPriceSpec = offer.priceSpecification.priceComponent[0];
             const itemOffered = offer.itemOffered;
             // 注文データのticketTypeに単価仕様が存在しないので、補完する
-            return Object.assign(Object.assign({}, itemOffered), { reservedTicket: Object.assign(Object.assign({}, itemOffered.reservedTicket), { ticketType: Object.assign(Object.assign({}, itemOffered.reservedTicket.ticketType), { priceSpecification: unitPriceSpec }) }) });
+            return Object.assign(Object.assign({}, itemOffered), { paymentNo: order.confirmationNumber, paymentMethod: (_a = order.paymentMethods[0]) === null || _a === void 0 ? void 0 : _a.name, reservedTicket: Object.assign(Object.assign({}, itemOffered.reservedTicket), { ticketType: Object.assign(Object.assign({}, itemOffered.reservedTicket.ticketType), { priceSpecification: unitPriceSpec }) }) });
         });
         // ↓動作確認がとれたら削除
         // if (!Array.isArray(reservations)) {
--- src/app/routes/reservations.ts
+++ src/app/routes/reservations.ts
@@ -35,6 +35,7 @@ const orderService = new cinerinoapi.service.Order({
  */
 reservationsRouter.get(
     '/print',
+    // tslint:disable-next-line:max-func-body-length
     async (req, res, next) => {
         try {
             // tslint:disable-next-line:no-suspicious-comment
@@ -43,21 +44,90 @@ reservationsRouter.get(
             // 他所からリンクされてくる時のためURLで言語を指定できるようにしておく (TTTS-230)
             (<any>req.session).locale = req.params.locale;
 
+            let orders: cinerinoapi.factory.order.IOrder[];
+            let reservations: tttsapi.factory.reservation.event.IReservation[];
+
             jwt.verify(<string>req.query.token, <string>process.env.TTTS_TOKEN_SECRET, async (jwtErr, decoded: any) => {
                 if (jwtErr instanceof Error) {
                     next(jwtErr);
                 } else {
+                    // 指定された予約ID
                     const ids = <string[]>decoded.object;
 
-                    let reservations = await Promise.all(ids.map(async (id) => reservationService.findById({ id })));
-                    reservations = reservations.filter(
-                        (r) => r.reservationStatus === tttsapi.factory.chevre.reservationStatusType.ReservationConfirmed
-                    );
+                    // decoded.reservationsが存在する場合に対応する
+                    if (Array.isArray(decoded.orders)) {
+                        // 注文番号と確認番号で注文照会
+                        const printingOrders: { orderNumber: string; confirmationNumber: string }[] = decoded.orders;
+                        orders = await Promise.all(printingOrders.map(async (printingOrder) => {
+                            const findOrderResult = await orderService.findByConfirmationNumber({
+                                confirmationNumber: String(printingOrder.confirmationNumber),
+                                orderNumber: String(printingOrder.orderNumber)
+                            });
 
-                    if (reservations.length === 0) {
-                        next(new Error(req.__('NotFound')));
+                            if (Array.isArray(findOrderResult)) {
+                                return findOrderResult[0];
+                            } else {
+                                return findOrderResult;
+                            }
+                        }));
 
-                        return;
+                        // 注文承認
+                        await Promise.all(orders.map(async (order) => {
+                            await orderService.authorize({
+                                object: {
+                                    orderNumber: order.orderNumber,
+                                    customer: { telephone: order.customer.telephone }
+                                },
+                                result: {
+                                    expiresInSeconds: CODE_EXPIRES_IN_SECONDS
+                                }
+                            });
+                        }));
+
+                        // 予約リストを抽出
+                        reservations = orders.reduce<tttsapi.factory.reservation.event.IReservation[]>(
+                            (a, b) => {
+                                const reservationsByOrder = b.acceptedOffers
+                                    // 指定された予約IDに絞る
+                                    .filter((offer) => {
+                                        return ids.includes((<cinerinoapi.factory.order.IReservation>offer.itemOffered).id);
+                                    })
+                                    .map((offer) => {
+                                        const unitPriceSpec = (<ICompoundPriceSpecification>offer.priceSpecification).priceComponent[0];
+
+                                        const itemOffered = <cinerinoapi.factory.order.IReservation>offer.itemOffered;
+
+                                        // 注文データのticketTypeに単価仕様が存在しないので、補完する
+                                        return <any>{
+                                            ...itemOffered,
+                                            paymentNo: b.confirmationNumber,
+                                            paymentMethod: b.paymentMethods[0]?.name,
+                                            reservedTicket: {
+                                                ...itemOffered.reservedTicket,
+                                                ticketType: {
+                                                    ...itemOffered.reservedTicket.ticketType,
+                                                    priceSpecification: unitPriceSpec
+                                                }
+                                            }
+                                        };
+                                    });
+
+                                return [...a, ...reservationsByOrder];
+
+                            },
+                            []
+                        );
+                    } else {
+                        reservations = await Promise.all(ids.map(async (id) => reservationService.findById({ id })));
+                        reservations = reservations.filter(
+                            (r) => r.reservationStatus === tttsapi.factory.chevre.reservationStatusType.ReservationConfirmed
+                        );
+
+                        if (reservations.length === 0) {
+                            next(new Error(req.__('NotFound')));
+
+                            return;
+                        }
                     }
 
                     renderPrintFormat(req, res)({ reservations });
@@ -131,6 +201,8 @@ reservationsRouter.get(
                 // 注文データのticketTypeに単価仕様が存在しないので、補完する
                 return <any>{
                     ...itemOffered,
+                    paymentNo: order.confirmationNumber,
+                    paymentMethod: order.paymentMethods[0]?.name,
                     reservedTicket: {
                         ...itemOffered.reservedTicket,
                         ticketType: {
